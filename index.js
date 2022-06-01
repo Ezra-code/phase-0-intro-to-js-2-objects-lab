bb0313c02bc37b619c3f96b2932715663c1eb6ed
Ezra-code <njerukithinji872@gmail.com>
2022-06-01 12:08:07 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+let employee = {
+    name:"Ezra",
+    streetAddress:"Kstreet"
+}
+// console.log(employee)
+// let fname = "name"
+// console.log(employee[fname])
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = {...employee}   
+    newObj[key] = value;
+    return newObj
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key){
+    const delObj = {...employee}
+    delete delObj[key]
+    return delObj
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
